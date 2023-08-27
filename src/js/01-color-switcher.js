acd81eefe17061783712eff4acff481db3c90e3d
MariuszZiental <122224840+MariuszZiental@users.noreply.github.com>
2023-08-27 22:02:49 +0200

    hw9 podejscie 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,40 @@
+const startBtn = document.querySelector('.data-start');
+const stopBtn = document.querySelector('.data-stop');
+const windowTheme = document.querySelector('body');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+startBtn.addEventListener('click', () => {
+  windowTheme.setInterval(() => {
+    windowTheme.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(windowTheme);
+  startBtn.disabled = false;
+});
+
+//
+//
+//
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+// const startBtn = document.querySelector('.js-start');
+// const stopBtn = document.querySelector('.js-stop');
+// let timerId = null;
+
+// startBtn.addEventListener('click', () => {
+//   timerId = setInterval(() => {
+//     console.log(`I love async JS!  ${Math.random()}`);
+//   }, 1000);
+// });
+
+// stopBtn.addEventListener('click', () => {
+//   clearInterval(timerId);
+//   console.log(`Interval with id ${timerId} has stopped!`);
+// });
