e9abdd64e5fe6ae1995c852c1f84fe723c388985
MariuszZiental <122224840+MariuszZiental@users.noreply.github.com>
2023-08-30 22:11:14 +0200

    hw9 podejscie 7

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,22 +1,3 @@
-// Opisany w dokumentacji
 import flatpickr from 'flatpickr';
-// Dodatkowy import stylów
 import 'flatpickr/dist/flatpickr.min.css';
-
-function flatpickr(selector, options) {}
-
-const date = new Date();
-
-console.log(date);
-// "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
-
-console.log(date.toString());
-// "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
-
-const teamMeetingDate = new Date('March 16, 2030');
-console.log(teamMeetingDate);
-// "Mon Mar 16 2030 00:00:00 GMT+0200 (Eastern European Standard Time)"
-
-const preciseTeamMeetingDate = new Date('March 16, 2030 14:25:00');
-console.log(preciseTeamMeetingDate);
-// "Mon Mar 16 2030 14:25:00 GMT+0200 (Eastern European Standard Time)"
+import Notiflix from 'notiflix';
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,6 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import Notiflix from 'notiflix';
+
+const elDelay = document.querySelector('input[name="delay"]');
+const elStep = document.querySelector('input[name="step"]');
+const elAmount = document.querySelector('input[name="amount"]');
+const elBtn = document.querySelector('button');
