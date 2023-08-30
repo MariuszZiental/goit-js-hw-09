3097133e4bb032595b9f9fbaf84210e1304760cc
MariuszZiental <122224840+MariuszZiental@users.noreply.github.com>
2023-08-30 14:12:41 +0200

    hw9 podejscie 3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,20 +1,22 @@
 const startBtn = document.querySelector('.data-start');
 const stopBtn = document.querySelector('.data-stop');
-const windowTheme = document.querySelector('body');
+const colorTheme = document.querySelector('body');
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+let timerId = null;
+
 startBtn.addEventListener('click', () => {
-  windowTheme.setInterval(() => {
-    windowTheme.style.backgroundColor = getRandomHexColor();
-  }, 1000);
   startBtn.disabled = true;
   stopBtn.disabled = false;
+  timerId.setInterval(() => {
+    colorTheme.style.backgroundColor = getRandomHexColor();
+  }, 1000);
 });
 
 stopBtn.addEventListener('click', () => {
-  clearInterval(windowTheme);
+  clearInterval(colorTheme);
   startBtn.disabled = false;
 });
 
