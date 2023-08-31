88d8dacafe2f2c4b5354edffcc85e850ca01471a
MariuszZiental <122224840+MariuszZiental@users.noreply.github.com>
2023-08-31 21:14:06 +0200

    hw9 3 start

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,3 +4,9 @@ const elDelay = document.querySelector('input[name="delay"]');
 const elStep = document.querySelector('input[name="step"]');
 const elAmount = document.querySelector('input[name="amount"]');
 const elBtn = document.querySelector('button');
+
+const createPromise = (position, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      
